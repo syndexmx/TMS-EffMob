05517cc75304a6b3bd8f321e731a389a28e64b2e
Maxim Shapovalov <syndex@list.ru>
2024-12-09 11:13:21 +0300

    Initial commit

---
+++ src/main/java/com/github/syndexmx/tmseffmob/TmseffmobApplication.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.tmseffmob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmseffmobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmseffmobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/syndexmx/tmseffmob/TmseffmobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.tmseffmob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmseffmobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
